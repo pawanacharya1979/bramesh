5f69cee0e60bc4b55fb0609a6e7a54d5280eb36d
Sunera <pawanacharya1979@gmail.com>
2019-03-22 10:55:06 +0530

    adding setting

--- manuroop/manuroop/settings.py
+++ manuroop/manuroop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -136,6 +136,6 @@ LOGIN_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+django_heroku.settings(locals())
